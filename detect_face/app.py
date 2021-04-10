0355239d9d28efe8a20b31b33b0d5850dd2415a2
Asher Gilani <gilaniasher@gmail.com>
2021-04-10 17:13:08 -0400

    Fixed cloud detect face code; now takes in base64 in the body and outputs coordinates

--- detect_face/app.py
+++ detect_face/app.py
@@ -1,18 +1,19 @@
-import json
+import base64
 import cv2
 import numpy as np
 
-def lambda_handler(event, context):
-    im_b64 = event['body']
+def face_detect(request):
+    # get image from base64
+    im_b64 = request.get_data()
     im_bytes = base64.b64decode(im_b64)
     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
     image = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
-    
+
     # Create the haar cascade
     cascPath = 'haarcascade_frontalface_default.xml'
     faceCascade = cv2.CascadeClassifier(cascPath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+
     # Detect faces in the image
     faces = faceCascade.detectMultiScale(
         gray,
@@ -21,17 +22,5 @@ def lambda_handler(event, context):
         minSize=(30, 30)
         #flags = cv2.CV_HAAR_SCALE_IMAGE
     )
-    
-    # print("Found {0} faces!".format(len(faces)))
-    
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    
-    # cv2.imshow("Faces found", image)
-    # cv2.waitKey(0)
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps(str(x, y, w, h))
-    }
+
+    return str(faces)
