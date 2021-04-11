ce1eb56cd1dc9c04190adc5beb0a542f8cd34fbd
unknown <jmmurray1099@gmail.com>
2021-04-10 22:58:40 -0400

    Updates

---
+++ precache-manifest.3fe75f8f02ff2baaa681c35e0bb49aea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c863a1876628c9100ca99320e8bf4270",
+    "url": "/starter-project/index.html"
+  },
+  {
+    "revision": "3a2395499d2548b63741",
+    "url": "/starter-project/static/css/main.4278a500.chunk.css"
+  },
+  {
+    "revision": "b5a7fb5b8627cc6ccb67",
+    "url": "/starter-project/static/js/2.b54fbd94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starter-project/static/js/2.b54fbd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2395499d2548b63741",
+    "url": "/starter-project/static/js/main.2152d8b5.chunk.js"
+  },
+  {
+    "revision": "8bb41a460e82067de101",
+    "url": "/starter-project/static/js/runtime-main.2758d215.js"
+  },
+  {
+    "revision": "a73d0ce551adf6c6a37481ed12336723",
+    "url": "/starter-project/static/media/Bitcamp_Project_Design.a73d0ce5.png"
+  },
+  {
+    "revision": "272e6086f5b51d587c48b9558579be90",
+    "url": "/starter-project/static/media/features-bg.272e6086.png"
+  }
+]);
